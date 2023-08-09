60c70727703ebd25a4c39596db65935357b111b7
Standley Gury (EXT DIGITAL) <standley.gury@petronas.com>
2023-08-09 14:28:22 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Sample from './components/sample-chart/sample'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Sample />
+    {/* the rest of the chart goes here */}
+    </>
+  )
+}
+
+export default App
---
+++ src/components/sample-chart/Sample.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import  './Sample.css';
+
+const Sample = () => {
+  return (
+    <div>Sample</div>
+  )
+}
+
+export default Sample
